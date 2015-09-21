e3f1705024e131b3284b2a12b0ab077c934ad956
Cheng Lian <lian@databricks.com>
2015-09-21 10:42:20 -0700

    Tests for the Profile example of PARQUET-364

--- src/test/scala/com/databricks/parquet/schema/ListCompatibilitySuite.scala
+++ src/test/scala/com/databricks/parquet/schema/ListCompatibilitySuite.scala
@@ -3,8 +3,9 @@ package com.databricks.parquet.schema
 import scala.collection.JavaConverters._
 
 import com.databricks.parquet.ParquetSuite
-import com.databricks.parquet.avro.AvroArrayOfOptionalInts
-import org.apache.avro.generic.GenericRecord
+import com.databricks.parquet.avro._
+import com.databricks.parquet.dsl._
+import org.apache.avro.generic.{GenericData, GenericRecord}
 
 class ListCompatibilitySuite extends ParquetSuite {
   test("PARQUET-364: read a Parquet record containing an Avro \"array<array<int>>\"") {
@@ -18,8 +19,6 @@ class ListCompatibilitySuite extends ParquetSuite {
           |  }
           |}
         """.stripMargin
-
-      import com.databricks.parquet.dsl._
 
       writeDirect(path.toString, schema) { implicit writer =>
         message { implicit consumer =>
@@ -67,8 +66,6 @@ class ListCompatibilitySuite extends ParquetSuite {
           |}
         """.stripMargin
 
-      import com.databricks.parquet.dsl._
-
       writeDirect(path.toString, schema) { implicit writer =>
         message { implicit consumer =>
           field(0, "f") {
@@ -99,6 +96,60 @@ class ListCompatibilitySuite extends ParquetSuite {
           Seq(2: Integer, 3: Integer).asJava).asJava
 
         assert(reader.read().get("f") === expected)
+      }
+    }
+  }
+
+  test("PARQUET-364: read a Parquet record containing an Avro array of single-element record") {
+    withTempHadoopPath { path =>
+      val schema =
+        """message root {
+          |  required group f (LIST) {
+          |    repeated group array {
+          |      required int32 element;
+          |    }
+          |  }
+          |}
+        """.stripMargin
+
+      writeDirect(path.toString, schema) { implicit writer =>
+        message { implicit consumer =>
+          field(0, "f") {
+            group {
+              field(0, "array") {
+                group { field(0, "element") { int(0) } }
+                group { field(0, "element") { int(1) } }
+              }
+            }
+          }
+        }
+      }
+
+      withAvroParquetReader[GenericRecord](path) { reader =>
+        val record = reader.read()
+        val f = record.get("f").asInstanceOf[GenericData.Array[GenericRecord]]
+        assert(f.get(0).get("element") === 0)
+        assert(f.get(1).get("element") === 1)
+      }
+    }
+  }
+
+  test("PARQUET-364: Profile example provided in parquet-dev list") {
+    withTempHadoopPath { path =>
+      val expected = {
+        val nest2 = Seq.tabulate(2) { i =>
+          val nest1 = Nest1.newBuilder().setMpi(0 + i).setTs(1 + i).build()
+          Nest2.newBuilder().setNest1(nest1).build()
+        }.asJava
+        Profile.newBuilder().setNest2(nest2).build()
+      }
+
+      withAvroParquetWriter[Profile](path, Profile.getClassSchema) { writer =>
+        writer.write(expected)
+      }
+
+      withAvroParquetReader[Profile](path) { reader =>
+        assert(reader.read() === expected)
       }
     }
   }
