a891efa6944dd54cb76033147d7cc45a61077936
Cheng Lian <lian@databricks.com>
2015-10-01 14:53:06 -0700

    Updates benchmark code

--- src/main/scala/com/databricks/parquet/examples/BlackholeBenchmark.scala
+++ src/main/scala/com/databricks/parquet/examples/BlackholeBenchmark.scala
@@ -4,7 +4,7 @@ import java.io.File
 
 import com.databricks.parquet.dsl.read
 import com.databricks.parquet.utils
-import org.scalameter.{Key, config}
+import com.google.common.base.Stopwatch
 import scopt.OptionParser
 
 /**
@@ -14,7 +14,30 @@ import scopt.OptionParser
 object BlackholeBenchmark {
   private val programName = BlackholeBenchmark.getClass.getSimpleName.stripSuffix("$")
 
-  private case class Config(path: Option[String] = None, runs: Int = 1, warmup: Boolean = false)
+  private case class Config(path: Option[String] = None, runs: Int = 1, warmupRuns: Int = 0)
+
+  // TODO Try JMH instead of homebrewed benchmark code
+  def benchmark(runs: Int, warmupRuns: Int = 0)(f: => Unit) {
+    val stopwatch = new Stopwatch()
+
+    (0 until warmupRuns).foreach { i =>
+      println(s"Warmup run $i...")
+      f
+    }
+
+    def run(i: Int) = {
+      stopwatch.reset()
+      stopwatch.start()
+      f
+      stopwatch.stop()
+      val elapsed = stopwatch.elapsedMillis()
+      println(s"Round $i: $elapsed ms")
+      elapsed
+    }
+
+    val total = (0 until runs).map(i => run(i)).sum.toDouble
+    println(s"Average: ${total / runs} ms")
+  }
 
   def main(args: Array[String]) {
     new OptionParser[Config](programName) {
@@ -23,10 +46,10 @@ object BlackholeBenchmark {
         .text("Specifies how many times to run the benchmark")
         .action { (runs, config) => config.copy(runs = runs) }
 
-      opt[Unit]('w', "warmup")
+      opt[Int]('w', "warmup-runs")
         .optional()
         .text("Requires JVM warmup")
-        .action { (_, config) => config.copy(warmup = true) }
+        .action { (runs, config) => config.copy(warmupRuns = runs) }
 
       arg[File]("<input-file>")
         .required()
@@ -34,11 +57,9 @@ object BlackholeBenchmark {
         .action { (file, config) => config.copy(path = Some(file.getCanonicalPath)) }
     }.parse(args, Config()).foreach { case Config(Some(path), runs, warmup) =>
       val schema = utils.schemaOf(path)
-      val time = config(Key.exec.benchRuns -> runs, Key.verbose -> warmup).measure {
+      benchmark(5) {
         read.blackhole(path, schema)
       }
-
-      println(s"Total time: $time")
     }
   }
 }
